80b11150f05e1047da41b08a180effc310741d25
dlwlrma123 <alexfinhart@gmail.com>
2022-04-10 19:09:11 +0800

    opkona: Enable `msm_irqbalance`
    Co-authored-by: Blackmanx <blackman@sajl.tk>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: DawfukFR <dawfukfr@gmail.com>

--- device.mk
+++ device.mk
@@ -263,6 +263,10 @@ PRODUCT_PACKAGES += \
     ueventd.oplus.rc \
     ueventd.qcom.rc
 
+# IRQ balance
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
