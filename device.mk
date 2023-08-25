192263476180cf76acd5e3308f4ae67a54841dfe
Abhishek Shukla <91975183+abhis699@users.noreply.github.com>
2023-08-25 15:02:41 +0530

    RMX3371: Move powerhint to DT
    Moving it to DT so that we can modify them later

--- device.mk
+++ device.mk
@@ -225,6 +225,7 @@ PRODUCT_PACKAGES += \
 # CameraGo V4
 PRODUCT_PACKAGES += \
     GCamGOPrebuilt-V4
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
@@ -383,6 +384,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.2.vendor
+
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # QMI
 PRODUCT_PACKAGES += \
