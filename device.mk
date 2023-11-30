fab023d9ce684e70feb35ad0eebdf161662ce05a
Abhishek Shukla <91975183+abhis699@users.noreply.github.com>
2023-11-30 15:41:25 +0530

    RMX3371: Build OplusCam

--- device.mk
+++ device.mk
@@ -358,6 +358,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.oplus.perf.rc
 
+# Camera
+$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
+
 # Overlays
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
